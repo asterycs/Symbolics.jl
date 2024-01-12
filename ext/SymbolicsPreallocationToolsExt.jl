fe718231538d1d15008b4d2f722b79ca45438948
Jimmy Envall <jimmyenvall@gmail.com>
2024-01-12 16:29:55 +0100

    Add more extension methods

--- ext/SymbolicsPreallocationToolsExt.jl
+++ ext/SymbolicsPreallocationToolsExt.jl
@@ -46,4 +46,48 @@ function get_tmp(dc::FixedSizeDiffCache, u::AbstractArray{X}) where {T,N, X<: Fo
     _restructure(dc.du, dc.any_du)
 end
 
+####
+
+function get_tmp(dc::DiffCache, u::Type{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
+function get_tmp(dc::DiffCache, u::X) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
+function get_tmp(dc::DiffCache, u::AbstractArray{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
+function get_tmp(dc::FixedSizeDiffCache, u::Type{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
+function get_tmp(dc::FixedSizeDiffCache, u::X) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
+function get_tmp(dc::FixedSizeDiffCache, u::AbstractArray{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
+    if length(dc.du) > length(dc.any_du)
+        resize!(dc.any_du, length(dc.du))
+    end
+    _restructure(dc.du, dc.any_du)
+end
+
 end
